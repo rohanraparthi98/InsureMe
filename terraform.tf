309bee44aa172b64610bab604f31775ec669ba1d
rohanraparthi98 <123003673+rohanraparthi98@users.noreply.github.com>
2023-07-12 22:24:55 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -128,7 +128,7 @@ resource "aws_eip" "proj-eip" {
 resource "aws_instance" "Prod-Server" {
  ami = "ami-053b0d53c279acc90"
  instance_type = "t2.micro"
- availability_zone = "us-east-1"
+ availability_zone = "us-east-1a"
  key_name = "rohan"
  network_interface {
  device_index = 0
